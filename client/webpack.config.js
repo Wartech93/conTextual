bc51477bfc27813da27a3aea00b84ac91b497933
Sweaty Hog <Royceatkins93@gmail.com>
2024-05-30 14:56:22 -0400

    got npm run start to start server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const webpack = require('webpack');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -19,11 +20,31 @@ module.exports = () => {
     },
     plugins: [
       new webpack.ProgressPlugin(),
-      new HtmlWebpackPlugin({
-        
+      new HtmlWebpackPlugin({        
         template: './index.html',
         title: 'Webpack Plugin'
-    })
+    }),
+    new InjectManifest({
+      swSrc: './src-sw',
+      swDest: 'service-worker.js',
+    }),
+  
+    new WebpackPwaManifest({
+      name: 'TODOs',
+      short_name: 'TODOs',
+      description: 'Keep track of important tasks!',
+      background_color: '#7eb4e2',
+      theme_color: '#7eb4e2',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
       
     ],
 
